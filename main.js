81cb173ffb165b4eab50ce41c16f20ca4a76afb9
yogapangestu228 <yogapangestu228@gmail.com>
2022-03-16 22:43:48 +0700

    Penambahan Object dan Array

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 //Data Primitif
 const namaLengkap = "Yoga Pangestu"; // String
-const umur = 23; // Number
-const mahasiswaAktif = false; //Boolean
-const status = undefined; // Undefined
-const koleksiFoto = null; // Null
+let umur = 23; // Number
+let mahasiswaAktif = false; //Boolean
+let status = undefined; // Undefined
+let koleksiFoto = null; // Null
 
+console.log(`${namaLengkap},${umur},${mahasiswaAktif},${status},${koleksiFoto}`);
 
+//Object
+const dataPribadi = {
+    namaLengkap : "Yoga Pangestu",
+    namaPanggilan : "Yopang",
+    jenisKelamin : "Laki-Laki",
+    agama : "Islam",
+    tempatLahir : "Tangerang",
+    tanggalLahir : "21 juli 1998",
+    alamat : {
+        jalan : "jl. Sekolah dusun 5",
+        kelurahan : "Purwodadi",
+        kecamatan : "Sunggal",
+        kabupaten : "Medan"
+    }
+};
+
+console.log(dataPribadi)
+
+//Array
+let belajarBinar = ['HTML','CSS','Layout','Responsive','Botstrap']
+
+console.log(belajarBinar)
+
+belajarBinar.push('Java script')
+
+console.log(belajarBinar)
