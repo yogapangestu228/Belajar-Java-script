9798e842c70e4067e2d0e9e89a59e4cb4612608c
yogapangestu228 <yogapangestu228@gmail.com>
2022-03-16 21:38:36 +0700

    Penambahan Data Primitif

---
+++ main.js
@@ -0,0 +1,8 @@
+//Data Primitif
+const namaLengkap = "Yoga Pangestu"; // String
+const umur = 23; // Number
+const mahasiswaAktif = false; //Boolean
+const status = undefined; // Undefined
+const koleksiFoto = null; // Null
+
+
